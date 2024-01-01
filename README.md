d16065d327b6f2fd46041038a0b22782f3ad036b
Isaias-DR <78326629+Isaias-DR@users.noreply.github.com>
2024-01-01 13:52:17 -0300

    0.1.0 Proyecto avanzado

---
+++ README.md
@@ -0,0 +1,3 @@
+# Creador de horarios
+
+El proceso parte en que Duoc UC semestralmente publica en la web los Excel de las sedes dispinibles, para que así los alumnos crear los horarios que deseen, de los cuales le ayudará tenerlos en la toma de ramos.
---
+++ js/no.js
@@ -0,0 +1,3 @@
+// (() => { debugger; alert('El código tiene licencia de código privado, solo el navegador puede usarlo. ') })();
+!1%-+~function(){debugger; alert('El código tiene licencia de código privado, solo el navegador puede usarlo. ') }();
+// +function(){debugger; alert('El código tiene licencia de código privado, solo el navegador puede usarlo. ') }();
